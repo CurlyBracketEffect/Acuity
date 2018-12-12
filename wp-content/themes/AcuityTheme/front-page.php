0fe230b76cdb87242920c58fab2c9c6cf24dadf1
CurlyBracketEffect <Jorrin.Bruns@gmail.com>
2018-12-12 10:11:30 -0500

    Update front-page.php to match jorrin branch

--- wp-content/themes/AcuityTheme/front-page.php
+++ wp-content/themes/AcuityTheme/front-page.php
@@ -10,27 +10,29 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <div class="hero-image">
-        <div class="hero-text">
-          <h2>Our Insights. Your Advantage.</h2>
-          <p>We are a different stategic avisor to GPs and LPs in the global private markets industry</p>
+      **TEST HOME PAGE..
 
-          <a href="#home-scroll" class="smooth-scroll">TEST SCROLL LINK<a>
-
-        </div>
+    <div class="hero-image">
+      <div class="hero-text">
+        <h2>Our Insights. Your Advantage.</h2>
+        <p>We are a different stategic avisor to GPs and LPs in the global private markets industry</p>
+        <a href="#home-scroll" class="smooth-scroll">TEST SCROLL LINK</a>
+          
       </div>
+    </div>
 
-      <!-- smooth scroll destination -->
-      <div id="home-scroll">TEST HOME SCROLL SECTION</div>
+    <!-- smooth scroll destination -->
 
-      <!-- back to top button -->
-      <div class="back-to-top">BACK TO TOP BUTTON</div>
+    <div id="home-scroll">TEST HOME SCROLL SECTION</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+    <!-- back to top button -->
+    <div class="back-to-top">BACK TO TOP BUTTON</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; // End of the loop. ?>
+      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
