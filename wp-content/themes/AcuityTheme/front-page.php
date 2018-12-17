3062a4ecf44864f05fd29c7b50f7337c082c9af0
navihothi <hothi.navi@gmail.com>
2018-12-17 10:17:15 -0500

    Working on experience page

--- wp-content/themes/AcuityTheme/front-page.php
+++ wp-content/themes/AcuityTheme/front-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      **TEST HOME PAGE..
+
 
     <div class="hero-image">
       <div class="hero-text">
--- wp-content/themes/AcuityTheme/header.php
+++ wp-content/themes/AcuityTheme/header.php
@@ -20,15 +20,13 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
-
-			THIS IS THE HEADER TEST
 			<header id="masthead" class="site-header" role="banner">
 
 				<div class = "header-container">
 					<div class="site-branding">
 						<div class = "acuity-logo">
 							<a href = <?php echo home_url('/'); ?>?>
-								<img src = "<?php echo get_template_directory_uri().'/assets/images/acuitylogo.png'; ?>"/> 
+								<img src = "<?php echo get_template_directory_uri().'/build/Assets/Images/acuitylogo.png'; ?>"/> 
 							</a>
 						</div> <!-- .acuity-logo -->
 					</div><!-- .site-branding -->
